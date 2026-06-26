81f9bbb9960c6c9ef7e06f3c8381fd7473b46351
Chetan Maurya <165866514+Chetan1maurya@users.noreply.github.com>
2026-06-26 15:52:35 +0530

    Update cookie settings for token management

--- virtualui-server/controllers/auth.controller.js
+++ virtualui-server/controllers/auth.controller.js
@@ -12,9 +12,9 @@ export const googleAuth = async(req,res) => {
         }
         let token = await genToken(user._id);
         res.cookie("token", token,{
-            httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            httpOnly: false,
+            secure: true,
+            sameSite: "none",
             maxAge: 7*24*60*60*1000
         })
         return res.status(200).json({
@@ -34,9 +34,9 @@ export const googleAuth = async(req,res) => {
 export const logout = async(req,res) => {
     try{
         await res.clearCookie("token",{
-            httpOnly: true,
-            secure: false,
-            sameSite: "strict"
+            httpOnly: false,
+            secure: true,
+            sameSite: "none"
         })
         return res.status(200).json({
             message: "Logged out successfully",
